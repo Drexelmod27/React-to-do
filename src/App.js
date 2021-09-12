4e74581172c792f185d1fcd2c8205438b8a57618
drexelmod27 <drexelmod27@gmail.com>
2021-09-12 14:14:59 -0700

    First Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+//app.js is a component that holds the logic in one file
+
+import React from 'react';
+import "./App.css"
+
+function App() {
+  return(
+    <div className="App">
+      <h1>Hello React</h1>
+    </div>
+  );
+}
+
+export default App;
