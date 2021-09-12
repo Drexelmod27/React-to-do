5d73dbb6509ff41576a307e687b09233311dc10b
drexelmod27 <drexelmod27@gmail.com>
2021-09-12 16:50:22 -0700

    added form and todolist javascript

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 //app.js is a component that holds the logic in one file
 
-import React from 'react';
+import React, { useState } from 'react';
 import "./App.css"
+//importing components
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  //value and the function that allows you to change the value
   return(
     <div className="App">
-      <h1>Hello React</h1>
+      <header>
+        <h1>Mark's To Do List</h1>
+      </header>
+      <Form setInputText={setInputText}/> 
+      <TodoList />
     </div>
   );
 }
